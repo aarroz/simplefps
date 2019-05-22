aefbfbe34a34689e405582b2f2f01fa82613b75e
Aaron <arroz@protonmail.com>
2019-05-21 20:24:58 -0700

    Updated to 3.1, following fps tutorial from docs

--- assets/simple_fpsplayer/Player.gd
+++ assets/simple_fpsplayer/Player.gd
@@ -2,43 +2,117 @@ extends KinematicBody
 #Variables
 var global = "root/global"
 
-var X = 0.00
-var Y = 0.00
+const GRAVITY = -32.8
+var vel = Vector3()
+const MAX_SPEED = 10
+const JUMP_SPEED = 12
+const ACCEL = 4.5
 
-var speed = 5
-var gravity = -9.8
-var moving = true
-var MOUSESPEED = 0.005
-var JUMP_VEL = 12
-var jumping = false
+var dir = Vector3()
 
-onready var playerfeet = get_node("playerfeet")
-onready var camera = get_node("Camera")
+const DEACCEL= 16
+const MAX_SLOPE_ANGLE = 40
+
+var camera
+var rotation_helper
+
+var MOUSE_SENSITIVITY = 0.05
+
+const MAX_SPRINT_SPEED = 20
+const SPRINT_ACCEL = 18
+var is_sprinting = false
+
+var flashlight
 
 func _ready():
+	camera = $rotation_helper/Camera
+	rotation_helper = $rotation_helper
+	flashlight = $rotation_helper/Flashlight
+	
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	set_physics_process(true)
-	set_process_input(true)
+
+func _physics_process(delta):
+	process_input(delta)
+	process_movement(delta)
+
+func process_input(delta):
+
+    # ----------------------------------
+    # Walking
+	dir = Vector3()
+	var cam_xform = camera.get_global_transform()
+
+	var input_movement_vector = Vector2()
+
+	if Input.is_action_pressed("movement_forward"):
+		input_movement_vector.y += 1
+	if Input.is_action_pressed("movement_backward"):
+		input_movement_vector.y -= 1
+	if Input.is_action_pressed("movement_left"):
+		input_movement_vector.x -= 1
+	if Input.is_action_pressed("movement_right"):
+		input_movement_vector.x += 1
+
+	input_movement_vector = input_movement_vector.normalized()
+
+	dir += -cam_xform.basis.z.normalized() * input_movement_vector.y
+	dir += cam_xform.basis.x.normalized() * input_movement_vector.x
+    # ----------------------------------
+
+    # ----------------------------------
+    # Jumping
+	if is_on_floor():
+		if Input.is_action_just_pressed("movement_jump"):
+			vel.y = JUMP_SPEED
+    # ----------------------------------
+
+    # ----------------------------------
+    # Capturing/Freeing the cursor
+	if Input.is_action_just_pressed("ui_cancel"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+    # ----------------------------------
+
+# ----------------------------------
+# Turning the flashlight on/off
+	if Input.is_action_just_pressed("flashlight"):
+		if flashlight.is_visible_in_tree():
+			flashlight.hide()
+		else:
+			flashlight.show()
+# ----------------------------------
+
+func process_movement(delta):
+	dir.y = 0
+	dir = dir.normalized()
+
+	vel.y += delta * GRAVITY
+
+	var hvel = vel
+	hvel.y = 0
+
+	var target = dir
+	target *= MAX_SPEED
+
+	var accel
+	if dir.dot(hvel) > 0:
+		accel = ACCEL
+	else:
+		accel = DEACCEL
+
+	hvel = hvel.linear_interpolate(target, accel * delta)
+	vel.x = hvel.x
+	vel.z = hvel.z
+	vel = move_and_slide(vel, Vector3(0, 1, 0), 0.05, 4, deg2rad(MAX_SLOPE_ANGLE))
 
 func _input(event):
-	# For Mouse Look. The Camera node has a script for the X rotation.
-	if event is InputEventMouseMotion:
-		rotation.y -= event.relative.x*MOUSESPEED
-	# Function to close game.
-	if Input.is_key_pressed(KEY_ESCAPE) or Input.is_key_pressed(KEY_Q):
-		get_tree().quit()
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		rotation_helper.rotate_x(deg2rad(event.relative.y * MOUSE_SENSITIVITY))
+		self.rotate_y(deg2rad(event.relative.x * MOUSE_SENSITIVITY * -1))
 
-func _process(delta):
-	var is_on_ground = playerfeet.is_colliding()
-	var on_top = playerfeet.get_collider()
-	move_and_collide(Vector3(0, gravity*delta, 0))
-	#Section for basic movement.
-	if Input.is_key_pressed(KEY_W):
-		translate(Vector3(0, 0, speed*delta))
-	if Input.is_key_pressed(KEY_S):
-		translate(Vector3(0, 0, -speed*delta))
-	if Input.is_key_pressed(KEY_A):
-		translate(Vector3(speed*delta, 0, 0))
-	if Input.is_key_pressed(KEY_D):
-		translate(Vector3(-speed*delta, 0, 0))
+		var camera_rot = rotation_helper.rotation_degrees
+		camera_rot.x = clamp(camera_rot.x, -70, 70)
+		rotation_helper.rotation_degrees = camera_rot
